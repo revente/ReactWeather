8a475565dd515f81974f03acd22852e74a8786c7
Andi <andi.lianto90@gmail.com>
2016-10-20 18:45:12 +0700

    change imperial unit to metric

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=9d35619d5e5ec7d3bd478ec4a6243d80&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=9d35619d5e5ec7d3bd478ec4a6243d80&units=metric';
 
 module.exports = {
   getTemp: function (location) {
